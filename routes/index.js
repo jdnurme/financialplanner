276547a2975f54493729f9916bcd046495249bf4
RUTH <RUTHPARK@MIT.EDU>
2017-06-04 13:33:29 -0400

    added index page

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,16 @@ var express = require('express');
 var router = express.Router();
 var session = require('express-session');
 
+/**
+ * get('/') renders the home page with all existing posts loaded
+ *
+ * @res-action - loads all posts
+ * 	@succes: renders homepage with all posts
+ *	@failure: renders error page with error message
+*/
+router.get('/', function(req, res, next) {
+	res.render('index', {title: "Financial Planner", message: "let's get our finances under control ;)"});
+});
 
 
 module.exports = router;
